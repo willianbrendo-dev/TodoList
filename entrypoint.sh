7974170796c2f5ea124da0dff536b6f60482437a
Willian Brendo <willianbrendo.dev@gmail.com>
2025-04-30 02:37:02 -0300

    ajuste para deploy9

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-echo "Esperando o banco de dados iniciar em $DB_HOST:$DB_PORT ..." # Mensagem melhorada
+echo "Esperando o banco de dados iniciar em $DB_HOST:$DB_PORT ..."
 
-while ! nc -z "$DB_HOST" "$DB_PORT"; do # Usando aspas para segurança
+while ! nc -z "$DB_HOST" "$DB_PORT"; do
   sleep 1
 done
 
@@ -10,7 +10,12 @@ echo "Banco de dados está pronto! Aplicando migrações..."
 
 python3 manage.py migrate
 
-echo "Migrações aplicadas. Iniciando o servidor Gunicorn..."
+# --- Adicione esta linha ---
+echo "Coletando arquivos estáticos..."
+python3 manage.py collectstatic --noinput # --noinput para não pedir confirmação
+# -------------------------
+
+echo "Migrações aplicadas e estáticos coletados. Iniciando o servidor Gunicorn..."
 
 # Substitua 'your_project_name' pelo nome real da pasta do seu projeto Django
-gunicorn core.wsgi:application --bind 0.0.0.0:8000
+gunicorn your_project_name.wsgi:application --bind 0.0.0.0:8000
