18a6d9ac59edfe1fb92d1fb0d17d3d88e30c793f
Willian Brendo <willianbrendo.dev@gmail.com>
2025-04-30 08:58:55 -0300

    ajuste para deploy13

--- core/settings.py
+++ core/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' 
 
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
