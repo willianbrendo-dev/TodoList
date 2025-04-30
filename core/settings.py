d332d3edffd3910fefae417d6fe38634b7515d13
Willian Brendo <willianbrendo.dev@gmail.com>
2025-04-30 09:11:24 -0300

    ajuste para deploy14

--- core/settings.py
+++ core/settings.py
@@ -121,8 +121,11 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' 
+STATICFILES_DIRS = [
+    STATIC_DIR, # Inclui o diretório BASE_DIR/static na busca do collectstatic
+]
 
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
